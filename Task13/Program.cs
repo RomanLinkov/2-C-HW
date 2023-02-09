0b18f5c648b90962ab8d9dfbbc7d493b53c30b64
Roman Linkov <roman.linkov@vk.com>
2023-02-09 17:11:52 +0300

    Задача 2 Начата

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System;
+
+Clear();
+Console.WriteLine("Здравствуйте!");
+Console.WriteLine("Введте трёхзначное натуральное число:");
+int N = Convert.ToInt32(ReadLine());
+
+int a1=N%10;
+int k=N/100;
+
+if (k<1)
+{
+    WriteLine("третьей цифры нет");
+}
+else 
+{
+    Write("третьея цифра ");
+    WriteLine(a1);
+} 

a667d911d91c72730681e0137a8cccbb28341558
Roman Linkov <roman.linkov@vk.com>
2023-02-09 17:47:41 +0300

    Задача 3 сделана

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using System;
+
+Clear();
+Console.WriteLine("Здравствуйте!");
+Console.WriteLine("Введите номер для недели от 1 до 7");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N<6)
+{
+    WriteLine("Это рабочий день.");
+}
+else 
+{
+    WriteLine("Это выходной день.");
+} 

297ad6bba6b8019d7755704a4c05f776c65f15a9
Roman Linkov <roman.linkov@vk.com>
2023-02-09 16:49:47 +0300

    Задача 1 сделана

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using System;
+
+Clear();
+Console.WriteLine("Салам алейкум, брат!");
+Console.WriteLine("Введи трёхзначное натуральное число, брат:");
+int N = Convert.ToInt32(ReadLine());
+
+int a1=N/10;
+int a2=a1%10;
+
+Write("Вторая цифра твоего числа: ");
+WriteLine(a2);
